d7c1ef7660ecd2c2b4d2d4289f3a548dceba822b
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-12-25 22:31:18 +0500

    learn basic of javascript

---
+++ Day-1/index.js
@@ -0,0 +1,58 @@
+// 1-> Javascript
+
+// Data Types
+
+// 1-> Primitive
+// 2-> Non Primitve
+
+// Primitive
+
+// String "hello world"
+// Number  5 8 9 100
+// Boolean true false
+// Null
+// undefined
+
+// Non Primitve
+
+// array
+// object
+// function
+
+// task 1 -> string concatenation
+
+// console.log(typeof ("7" + 7 + 7));
+
+// task 2 -> Getting Character
+
+// var name = "Bhopindra jogi"; // B -> 0 h -> 1
+// console.log(name[1]);
+
+
+// task 3 Log length
+
+// console.log(name.slice(9, 14));
+
+// console.log(5**5);
+
+// += -= compound operators
+// ++ -- increment/ decrement operators
+
+// var num;
+// // num += 1;
+// console.log(num);
+
+// [1,2,3,4,5,6,7,8,9,0]
+// ["A", "B", "C"]
+// [true, false]
+
+// var arr = [1, 2, 3];
+// arr.push(4);
+// arr.pop();
+// arr.pop();
+// arr.unshift(0)
+// arr.shift()
+// console.log(arr.concat())
+// console.log(arr.indexOf(3))
+
+// console.log(arr);
