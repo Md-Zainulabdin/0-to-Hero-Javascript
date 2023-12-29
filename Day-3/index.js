a969b2a2bb91f2367a53306486d75ad79d49c9f5
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-12-29 21:20:38 +0500

    day 3

---
+++ Day-3/index.js
@@ -0,0 +1,72 @@
+// console.log("Hello World");
+
+// callback function
+
+// function greet(callback) { // Regular Function / Function Decleration
+//     callback();
+// }
+
+
+// greet(() => console.log("Callback Function")); // arrow function
+
+// setTimeOut
+// setInterval
+
+// setTimeout(() => {
+//     console.log("Hello world");
+// }, 2000)
+
+// setInterval(() => {
+//     console.log("Hello World");
+// }, 1000)
+
+// for each
+
+// let updatedArray = arr.forEach((i) => {
+//     console.log(i);
+// })
+
+// Map
+
+// [2 , 4, 6 ]
+
+// let doubledArray = arr.map((i) => i + i);
+// console.log(doubledArray);
+
+// filter
+// const isEven = arr.filter((i) => !(i % 2 == 0));
+// console.log(isEven);
+
+
+// find
+
+// const findedNumber = arr.filter((i) => i == 4);
+// console.log(findedNumber);
+
+// Object
+
+// let arr = [1, 2, 3, 4, 5, 6, 4, 7, 8];
+// let student = {
+//     name: "zain",
+//     age: 18,
+//     male: true,
+//     blogs: ["A", "B", "C"],
+// }
+
+// for (let key in student) {
+//     console.log(key + student[key]);
+// }
+
+// for (let index of arr) {
+//     console.log(index);
+// }
+
+// console.log(student.paid)
+// student.greet();
+
+// let num = 5.90;
+
+// let randomNumber = Math.random();
+// console.log(randomNumber);
+
+// console.log(Math.trunc(num));
